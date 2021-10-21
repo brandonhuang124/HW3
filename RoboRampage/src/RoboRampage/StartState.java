399a6b121c4c55b8cd497a5d7f14109fbd572147
Brandon Huang <brandonhuang124@gmail.com>
2021-10-21 00:39:41 -0700

    Added basic menu screen.

--- RoboRampage/src/RoboRampage/StartState.java
+++ RoboRampage/src/RoboRampage/StartState.java
@@ -10,6 +10,8 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class StartState extends BasicGameState {
 
+  private int select, timer;
+  private boolean selected, arrowBlink;
   @Override
   public int getID() {
     return RoboGame.STARTUPSTATE;
@@ -24,11 +26,32 @@ public class StartState extends BasicGameState {
   public void enter(GameContainer container, StateBasedGame game) {
     RoboGame rg = (RoboGame)game;
     container.setSoundOn(true);
+    select = 0;
+    selected = false;
+    arrowBlink = true;
   }
 
   @Override
   public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-
+    // Draw Menu splash and buttons
+    g.drawImage(ResourceManager.getImage(RoboGame.MENU_SPLASH_RSC), 0, 0);
+    g.drawImage(ResourceManager.getImage(RoboGame.MENU_START_RSC), 290, 450);
+    g.drawImage(ResourceManager.getImage(RoboGame.MENU_HOWTOPLAY_RSC), 210, 520);
+    g.drawImage(ResourceManager.getImage(RoboGame.MENU_LEVELSELECT_RSC), 183, 590);
+    // Draw Arrow Location
+    if (arrowBlink) {
+      switch(select) {
+        case 1:
+          g.drawImage(ResourceManager.getImage(RoboGame.MENU_ARROW_RSC), 590, 530);
+          break;
+        case 2:
+          g.drawImage(ResourceManager.getImage(RoboGame.MENU_ARROW_RSC), 617, 600);
+          break;
+        default:
+          g.drawImage(ResourceManager.getImage(RoboGame.MENU_ARROW_RSC), 510, 460);
+          break;
+      }
+    }
   }
 
   @Override
@@ -36,7 +59,49 @@ public class StartState extends BasicGameState {
 
     Input input = container.getInput();
     RoboGame rg = (RoboGame)game;
-    // Goto test state
-    rg.enterState(RoboGame.TESTSTATE);
+
+    // Checking if a selection has been made
+    if(selected) {
+      if(timer % 15 == 0) {
+        arrowBlink = !arrowBlink;
+      }
+      // Once wait period is over
+      if(timer <= 0) {
+        switch(select) {
+          case 1:
+            System.out.println("Nothing here yet!");
+            selected = false;
+            arrowBlink = true;
+            break;
+          case 2:
+            System.out.println("No Level Select Screen yet!");
+            selected = false;
+            arrowBlink = true;
+            break;
+          default:
+            System.out.println("Game starting...");
+            rg.enterState(RoboGame.TESTSTATE);
+            break;
+        }
+      }
+      timer --;
+    }
+
+    // If Space is pressed to select
+    else if(input.isKeyPressed(Input.KEY_SPACE)) {
+      timer = 75;
+      selected = true;
+    }
+    // If moving down
+    else if(input.isKeyPressed(Input.KEY_D) || input.isKeyPressed(Input.KEY_DOWN)) {
+      select ++;
+    }
+    // If moving up
+    else if(input.isKeyPressed(Input.KEY_W) || input.isKeyPressed(Input.KEY_UP)) {
+      select --;
+    }
+    // Mod select by 3 to snap back to top, set negatives to 2 to snap to the bottom when going up at the top.
+    select = select % 3;
+    if(select < 0) select = 2;
   }
 }
