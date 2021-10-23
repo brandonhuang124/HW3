c5ea6b86b950912d24268b5331c0544a9aaf5775
Brandon Huang <brandonhuang124@gmail.com>
2021-10-23 05:30:06 -0700

    Comment cleanup on RoboGame, Coordinate, Crosshair, Tile, and Vertex

--- RoboRampage/src/RoboRampage/Crosshair.java
+++ RoboRampage/src/RoboRampage/Crosshair.java
@@ -4,13 +4,37 @@ import jig.Entity;
 import jig.ResourceManager;
 import jig.Vector;
 
+/***
+ * Crosshair entity which shows which direction the player is aiming in. Does not interact with the environment in any
+ * way and stays in the tiles adjacent to the player.
+ *
+ * Methods:
+ *  moveLeft()
+ *  moveUp()
+ *  moveDown()
+ *  moveRight()
+ */
+
 public class Crosshair extends Entity {
 
+  /**
+   * Constructor,
+   * @param x
+   *  x coordinate to spawn in (absolute, not tile coordinates.)
+   * @param y
+   *  y coordinate to spawn in (absolute, not tile coordinates.)
+   */
   public Crosshair (final float x, final float y) {
     super(x,y);
     addImage(ResourceManager.getImage(RoboGame.UTIL_CROSSHAIRIMG_RSC));
   }
 
+  /**
+   * The four methods below move the crosshair to the corresponding direction in their titles relative to the player.
+   *
+   * @param playerloc
+   *  Location of the player the crosshair needs to be placed by
+   */
   public void moveLeft(Coordinate playerloc) {
     this.setPosition(((playerloc.x - 1) * 75) + 37, (playerloc.y * 75) + 37);
   }
